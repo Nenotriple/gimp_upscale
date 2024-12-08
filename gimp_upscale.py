9a085efe9f78e574c2106797f4ddac533e45d9eb
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2024-12-08 15:51:31 -0800

    Refactor image upscaling logic by extracting selection and layer handling into separate functions

--- gimp_upscale.py
+++ gimp_upscale.py
@@ -111,13 +111,12 @@ def _run_resrgan(temp_input_file, temp_output_file, model):
     '''Upscale the image using the RESRGAN executable'''
     script_dir = os.path.dirname(os.path.realpath(__file__))
     resrgan_exe = os.path.join(script_dir, "resrgan/realesrgan-ncnn-vulkan.exe")
-    # Run the external command to upscale the image
     upscale_process = subprocess.Popen([
         resrgan_exe,
         "-i", temp_input_file,
         "-o", temp_output_file,
         "-n", model
-        ])
+    ])
     upscale_process.wait()
 
 
@@ -126,34 +125,37 @@ def _load_upscaled_image(image, drawable, temp_output_file, output_factor, upsca
     upscaled_image = pdb.gimp_file_load(temp_output_file, temp_output_file)
     upscaled_layer = pdb.gimp_image_get_active_layer(upscaled_image)
     if upscale_selection:
-        # Get the coordinates of the original selection
-        x1, y1, x2, y2 = pdb.gimp_selection_bounds(image)[1:]
-        sel_width = x2 - x1
-        sel_height = y2 - y1
-        # Scale the upscaled layer to match the selection size
-        pdb.gimp_layer_scale(upscaled_layer, sel_width, sel_height, False)
-        # Create a new layer for the upscaled selection
-        new_layer = pdb.gimp_layer_new(image, pdb.gimp_drawable_width(drawable), pdb.gimp_drawable_height(drawable), RGBA_IMAGE, "Upscaled Selection", 100, NORMAL_MODE)
-        pdb.gimp_image_insert_layer(image, new_layer, None, -1)
-        # Paste the upscaled content into the new layer
-        pdb.gimp_edit_copy(upscaled_layer)
-        floating_sel = pdb.gimp_edit_paste(new_layer, False)
-        pdb.gimp_layer_set_offsets(floating_sel, x1, y1)
-        pdb.gimp_floating_sel_anchor(floating_sel)
+        _handle_upscaled_selection(image, drawable, upscaled_layer)
     else:
-        # Resize the original image canvas based on the output factor
-        orig_width = pdb.gimp_image_width(image)
-        orig_height = pdb.gimp_image_height(image)
-        output_width = int(orig_width * output_factor)
-        output_height = int(orig_height * output_factor)
-        pdb.gimp_image_resize(image, output_width, output_height, 0, 0)
-        # Scale the upscaled image layer to match the new canvas size
-        pdb.gimp_layer_scale(upscaled_layer, output_width, output_height, False)
-        # Insert the upscaled layer into the image
-        upscaled_layer_resized = pdb.gimp_layer_new_from_drawable(upscaled_layer, image)
-        pdb.gimp_image_insert_layer(image, upscaled_layer_resized, None, -1)
+        _handle_upscaled_layer(image, drawable, upscaled_layer, output_factor)
     # Clean up the temporary upscaled image
     pdb.gimp_image_delete(upscaled_image)
+
+
+def _handle_upscaled_selection(image, drawable, upscaled_layer):
+    '''Handles the upscaled image when upscaling a selection'''
+    x1, y1, x2, y2 = pdb.gimp_selection_bounds(image)[1:]
+    sel_width = x2 - x1
+    sel_height = y2 - y1
+    pdb.gimp_layer_scale(upscaled_layer, sel_width, sel_height, False)
+    new_layer = pdb.gimp_layer_new(image, pdb.gimp_drawable_width(drawable), pdb.gimp_drawable_height(drawable), RGBA_IMAGE, "Upscaled Selection", 100, NORMAL_MODE)
+    pdb.gimp_image_insert_layer(image, new_layer, None, -1)
+    pdb.gimp_edit_copy(upscaled_layer)
+    floating_sel = pdb.gimp_edit_paste(new_layer, False)
+    pdb.gimp_layer_set_offsets(floating_sel, x1, y1)
+    pdb.gimp_floating_sel_anchor(floating_sel)
+
+
+def _handle_upscaled_layer(image, drawable, upscaled_layer, output_factor):
+    '''Handles the upscaled image when upscaling a layer'''
+    orig_width = pdb.gimp_image_width(image)
+    orig_height = pdb.gimp_image_height(image)
+    output_width = int(orig_width * output_factor)
+    output_height = int(orig_height * output_factor)
+    pdb.gimp_image_resize(image, output_width, output_height, 0, 0)
+    pdb.gimp_layer_scale(upscaled_layer, output_width, output_height, False)
+    upscaled_layer_resized = pdb.gimp_layer_new_from_drawable(upscaled_layer, image)
+    pdb.gimp_image_insert_layer(image, upscaled_layer_resized, None, -1)
 
 
 def _cleanup_temp_files(image, selected_layer, temp_input_file, temp_output_file, upscale_selection, keep_copy_layer):
